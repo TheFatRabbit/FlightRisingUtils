505f41f85162bc7df717a9aac3c22f1855796920
TheFatRabbit <pvignaly@gmail.com>
2023-12-02 19:57:29 -0500

    globals.py

---
+++ globals.py
@@ -0,0 +1,70 @@
+VENUE_NAMES = (
+    "Training Fields",
+    "Woodland Path",
+    "Scorched Forest",
+    "Sandswept Delta",
+    "Silk-Strewn Wreckage",
+    "Blooming Grove",
+    "Forgotten Cave",
+    "Bamboo Falls",
+    "Thunderhead Savanna",
+    "Redrock Cove",
+    "Waterway",
+    "Arena",
+    "Volcanic Vents",
+    "Rainsong Jungle",
+    "Boreal Wood",
+    "Crystal Pools",
+    "Harpy's Roost",
+    "Ghostlight Ruins",
+    "Mire",
+    "Kelp Beds",
+    "Golem Workshop",
+    "Forbidden Portal"
+)
+
+DEFAULT_VENUE = "Redrock Cove"
+
+LOOT_TITLE_BOUNDS = (1250, 450, 1367, 507)
+
+LOOT_ITEM_BOUNDS = (1105, 511, 1340, 745)
+
+COLI_SCREEN_BOUNDS = (700, 400, 1400, 930)
+
+LEFT_SCREEN_BBOX = ((0, 100), (466, 1079))
+
+GENERIC_CHESTS = (
+    "Brightshine Jubilee Chest",
+    "Crystalline Gala Chest", 
+    "Greenskeeper Gathering Chest",
+    "Flameforger's Festival Chest",
+    "Mistral Jamboree Chest",
+    "Riot of Rot Chest",
+    "Rockbreaker's Ceremony Chest",
+    "Starfall Celebration Chest",
+    "Thundercrack Carnivale Chest",
+    "Trickmurk Circus Chest",
+    "Wavecrest Saturnalia Chest",
+    "Arena Crate",
+    "Bamboo Falls Crate",
+    "Blooming Grove Crate",
+    "Boreal Wood Crate",
+    "Crystal Pools Crate",
+    "Forbidden Portal Crate",
+    "Forgotten Cave Crate",
+    "Ghostlight Ruins Crate",
+    "Golem Workshop Crate",
+    "Harpy's Roost Crate",
+    "Kelp Beds Crate",
+    "Mire Crate",
+    "Rainsong Jungle Crate",
+    "Redrock Cove Crate",
+    "Sandswept Delta Crate",
+    "Scorched Forest Crate",
+    "Silk-Strewn Wreckage Crate",
+    "Thunderhead Savanna Crate",
+    "Training Fields Crate",
+    "Volcanic Vents Crate",
+    "Waterway Crate",
+    "Woodland Path Crate"
+)
