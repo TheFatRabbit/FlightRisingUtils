763b86ed057f7bc62615144dd7557cb3b2978a24
Phillip Vignaly <pvignaly@gmail.com>
2024-12-19 21:15:01 -0500

    strange tracker changes refactor buttons to make easily mutable add print info button

--- strange_tracker.py
+++ strange_tracker.py
@@ -1,30 +1,29 @@
 import tkinter
 
-def increment_widget_value(widget):
-    widget_str = widget.cget("text")
-    widget_num = int(widget_str[widget_str.index(": ")+2:]) + 1
-    widget_str = widget_str[:widget_str.index(": ")+2]
-    widget.config(text=f"{widget_str}{widget_num}")
+categories = ("Familiars", "Apparel", "Genes", "Scenes", "Vistas", "Eggs", "Scrolls")
 
 gui = tkinter.Tk()
 gui.title("Strange Chest Tracker")
 gui.geometry("300x300")
 gui.attributes("-topmost", True)
 
-fam_btn = tkinter.Button(gui, text="Familiars: 0", command=lambda: increment_widget_value(fam_btn))
-apparel_btn = tkinter.Button(gui, text="Apparel: 0", command=lambda: increment_widget_value(apparel_btn))
-gene_btn = tkinter.Button(gui, text="Genes: 0", command=lambda: increment_widget_value(gene_btn))
-scene_btn = tkinter.Button(gui, text="Scenes: 0", command=lambda: increment_widget_value(scene_btn))
-vista_btn = tkinter.Button(gui, text="Vistas: 0", command=lambda: increment_widget_value(vista_btn))
-egg_btn = tkinter.Button(gui, text="Eggs: 0", command=lambda: increment_widget_value(egg_btn))
-scroll_btn = tkinter.Button(gui, text="Scrolls: 0", command=lambda: increment_widget_value(scroll_btn))
+def increment_widget_value(widget):
+    widget_str = widget.cget("text")
+    widget_num = int(widget_str[widget_str.index(": ")+2:]) + 1
+    widget_str = widget_str[:widget_str.index(": ")+2]
+    widget.config(text=f"{widget_str}{widget_num}")
 
-fam_btn.pack()
-apparel_btn.pack()
-gene_btn.pack()
-scene_btn.pack()
-vista_btn.pack()
-egg_btn.pack()
-scroll_btn.pack()
+def print_info():
+    s = ""
+    loots = [b.cget("text") for b in increment_btns]
+    for loot in loots:
+        s += f"{loot[loot.index(': ')+2:]}\n"
+    print(s)
+
+increment_btns = [tkinter.Button(gui, text=f"{n}: 0", command=lambda i=i: increment_widget_value(increment_btns[i])) for i, n in enumerate(categories)]
+for b in increment_btns: b.pack()
+
+print_btn = tkinter.Button(gui, text="Print Info", command=print_info)
+print_btn.pack()
 
 gui.mainloop()
