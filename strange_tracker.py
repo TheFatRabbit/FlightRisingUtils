338800151bea32b5ac7933f774c184a7ab7b908d
Phillip Vignaly <pvignaly@gmail.com>
2024-12-19 21:34:22 -0500

    add copy to clipboard functionality to strange tracker

--- strange_tracker.py
+++ strange_tracker.py
@@ -13,17 +13,18 @@ def increment_widget_value(widget):
     widget_str = widget_str[:widget_str.index(": ")+2]
     widget.config(text=f"{widget_str}{widget_num}")
 
-def print_info():
+def print_and_copy():
     s = ""
     loots = [b.cget("text") for b in increment_btns]
     for loot in loots:
         s += f"{loot[loot.index(': ')+2:]}\n"
     print(s)
+    gui.clipboard_append(s)
 
 increment_btns = [tkinter.Button(gui, text=f"{n}: 0", command=lambda i=i: increment_widget_value(increment_btns[i])) for i, n in enumerate(categories)]
 for b in increment_btns: b.pack()
 
-print_btn = tkinter.Button(gui, text="Print Info", command=print_info)
+print_btn = tkinter.Button(gui, text="Print & Copy", command=print_and_copy)
 print_btn.pack()
 
 gui.mainloop()
